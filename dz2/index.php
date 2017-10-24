c5fff1085f2992e0ded5e582b2719535b8da3561
nikchest <n.shelestov@gmail.com>
2017-10-24 19:54:27 +0300

    Мелкие правки

--- dz2/index.php
+++ dz2/index.php
@@ -3,9 +3,9 @@ require('functions.php');
 
 echo task1($arr_str, true);
 task2($arr_num, $sign);
-task3('+', 1, 2, 3, 5.2);
+task3('/', 1, 2, 3, 5.2);
 task4(4,4);
-task5_2();
+task5_2('3топот3');
 task6();
 task7('Карл у Клары украл Кораллы');
 task7_1('Две бутылки лимонада');
